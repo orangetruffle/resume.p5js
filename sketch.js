e6ad6aca62a8e2b4d1e90ec0eae937b802b61c26
orangetruffle <ellebassett@protonmail.com>
2023-02-18 14:46:56 +0000

    Basis of my Resume, with moving sine shape and capture libarary!

--- sketch.js
+++ sketch.js
@@ -1,44 +1,60 @@
 /**
- * Sinusodial Sketch
+ * Sinusodial Sketch.
+ * Designed and Developed by Elle Bassett for Generative Drawing.
+ * ***************************************************************
  */
+let overlay
 
+function preload() {
+  overlay = loadImage("./resumeOverlay.png");
+}
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  colorMode(HSB);
+  createCanvas(2480, 3508);
+  noFill();
+  
 }
 
 function draw() {
-  background(255);
+  
+  // if(frameCount === 1){
+    // capturer.start();
+  // }
 
+  background("#D3C4E3");
+  
+push();
+  beginShape()
+  translate(width/2.65, height/1.65);
+  for(var i = 0; i <= 359; i++) {
+    /**
+     * rMin/Max provides the animation for the shape. 
+     * Make sure collective sum of Max does not exceed canvas Width.  
+     * Collective sum of Min
+     */
 
+    var r1Min = map(cos(frameCount/200), -1, 1, -10*2, 211*2);
+    var r1Max = map(sin(frameCount/180), -1, 1, 97 *2, -50*2);
 
-  var points = [];
-  translate(width/2, height/2);
-
-  beginShape();
-  for(var i = 0; i < 360; i++) {
- 
-    stroke(200,100,50);
-    noFill();
-    var r1Min = map(cos(frameCount/30), -1, 1, 400, -100);
-    var r1Max = map(sin(frameCount/25), -1, 1, 200, -50);
-
-    var r2Min = map(sin(frameCount/15), -1, 1, 200, 100);
-    var r2Max = map(cos(frameCount/15), -1, 1, 100, 200);
+    var r2Min = map(sin(frameCount/220), -1, 1, -20*2, 300*2);
+    var r2Max = map(cos(frameCount/220), -1, 1, 100*2.5, 300*2);
 
     var r1 = map(sin(i*3), -1, 1, r1Min, r1Max);
     var r2 = map(sin(i*6), -1, 1, r2Min, r2Max);
     var r = r1 + r2;
     var x = r * cos(i);
     var y = r * sin(i);
-    vertex(x,y);
-    points.push([x,y]);
+      
+    stroke("#E68C8C");
+    vertex(x,y);  
+    
   }
-
-  // for (var i = 0; i < points.length-1; i++){
-    // draw line between points[i] and points[i+1]
-// 
+  endShape();
+pop();
+  // if(frameCount < 21600) {
+    // capturer.capture(canvas);
+  // } else if(frameCount === 21600) {
+    // capturer.stop();
+    // capturer.save();
   // }
-  endShape(CLOSE);
-   
+  image(overlay,0,0);
 }
